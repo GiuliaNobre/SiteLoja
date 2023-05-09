fc2db7da7505632e35160957de1bc8767f1df33e
GiuliaNobre <giuliarnobre@gmail.com>
2023-05-09 11:02:52 -0300

    add

--- App.vue
+++ App.vue
@@ -1,6 +1,13 @@
 <script setup>
 import { ref } from 'vue'
 
+const novoItem = ref({
+    id: 0,
+    nome: '',
+    preco: 0,
+    quantidade: 1,
+})
+
 const produtos = ref([
     {
         id: 1,
@@ -52,121 +59,147 @@ const produtos = ref([
     },
     {
         id: 9,
-        nome: 'Cueca',
-        preco: 19.90,
-        quantidade: 0
-    },
-    {
-        id: 10,
         nome: 'Meia',
         preco: 9.90,
         quantidade: 0
     }
 ])
-
-const carrinho = [
+const carrinho = ref([
     {
-        id: 1,
         nome: 'Camiseta',
         preco: 49.90,
-        quantidadeCarrinho: 1,
+        quantidade: 1,
         valorTotal: 49.90
     },
     {
-        id: 2,
         nome: 'Calça',
         preco: 99.90,
-        quantidadeCarrinho: 2,
+        quantidade: 2,
         valorTotal: 199.80
     },
     {
-        id: 3,
         nome: 'Meia',
         preco: 9.90,
-        quantidadeCarrinho: 4,
+        quantidade: 4,
         valorTotal: 39.60
     }
-]
+])
 
-function total() {
-    for (let i = 0; i < carrinho.length; i++) {
-        const valorTotal = valorTotal[i] + valorTotal;
+
+function remover(index) {
+    carrinho.value.splice(index, 1);
+}
+
+function decrementar(id) {
+    if (produtos.value[id].quantidade > 0) {
+        produtos.value[id].quantidade--;
     }
 }
 
-function diminuir(idProd) {
-    if (produtos.value[idProd].quantidade > 0) {
-        produtos.value[idProd].quantidade--
+function adicionar(index) {
+    if(produtos.value[index].quantidade > 0){
+
+        novoItem.value.id = produtos.value[index].id;
+        novoItem.value.nome = produtos.value[index].nome;
+        novoItem.value.preco = produtos.value[index].preco;
+        novoItem.value.quantidade = produtos.value[index].quantidade;
+        carrinho.value.push({ ...novoItem.value });
+    }else{
+        alert("Selecione a quantidade do produto desejado")
     }
+
 }
 
-function adicionarCarrinho() {
-    carrinho.value.push(produtos.value);
-    carrinho.value = ''
-}
-
-function removerCarrinho(key) {
-    carrinho.value.splice(index, 1)
-}
 
 </script>
+
 <template>
+    <div class="geral">
 
-    
-    <ul>
-        <h2>Produtos</h2>
-        <li v-for="(produto, index) in produtos" :key="index">
-            <p>Nome: {{ produto.nome }}</p>
-            <p>Preço: R$:{{ produto.preco }}</p>
-            <p>Quantidade: {{ produto.quantidade }}</p>
-            <p v-for="(produto, key) in produtos" :key="key">{{ produto.key }}</p>
-            <button @click="produto.quantidade++">+</button>
-            <button @click="diminuir(produto.id-1)">-</button>
-        </li>
-    </ul>
-
-    <div class="container">
-        <h2>Carrinho</h2>
-        <div class="item" v-for="(item, index) in carrinho" :key="index">
-            <p>Nome: {{ item.nome }}</p>
-            <p>Preço: R$:{{ item.preco }}</p>
-            <p>Quantidade: {{ item.quantidade }}</p>
+        <div class="produtos">
+            <h1>Produtos</h1>
+            <div class="gridProdutos">
+                <div class="itens" v-for="(produto, index) in produtos" :key="index">
+                    <p>Nome: {{ produto.nome }}</p>
+                    <p>Preço: R$ {{ produto.preco }}</p>
+                    <p>Quantidade: {{ produto.quantidade }}</p>
+                    <button @click="produto.quantidade++">+</button>
+                    <button @click="decrementar(produto.id - 1)">-</button>
+                    <button @click="adicionar(index)">Adicionar</button>
+                </div>
+            </div>
         </div>
-        <button @click="total" class="botao">Somar produtos</button>
-        <p>{{ valorTotal }}</p>
-            <button type="submit" class="botao">finalizar compra</button>
+
+
+        <div class="carrinho">
+            <h2>Carrinho</h2>
+            <div class="itens" v-for="(produto, index) in carrinho" :key="index">
+                <p>Nome: {{ produto.nome }}</p>
+                <p>Preço: R$ {{ produto.preco }}</p>
+                <p>Quantidade: {{ produto.quantidade }}</p>
+                <button @click="remover(index)">Remover</button>
+
+            </div>
+        </div>
     </div>
 </template>
+
 <style scoped>
-.botao {
-    border: none;
-    border-radius: 5px;
-    font-weight: bold;
-    font-size: 16px;
-    padding: 7px 20px;
-    background-color: #595C48;
-    color: aliceblue;
-    margin: 5px;
-    width: 220px;
+.produtos, .carrinho {
+    text-align: center;
+    margin: 20px;
+    margin-top: 20px;
+    width: auto;
+    height: min-content;
+
+    border-radius: 6px;
+    color: #F2F2F2;
 }
-.container {
-    font-size: 18px;
-    background-color: #253B35;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 80px;
-    border-radius: 15px;
-    box-shadow: 0px 10px 40px #00000056;
-    width: 400px;
-    color: aliceblue;
-  }
 
-  li {
-    list-style: none;
-  }
+.gridProdutos {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
 
-.item {
-    background-color: #253B35;
+}
+
+.itens {
+    
+    width: 200px;
+    padding: 10px;
+    background-color: #262626;
+    box-shadow: #262626 2px 2px 10px;
+    margin: 20px auto;
+    font-size: 16px;
+    border-radius: 6px;
+}
+
+
+.geral {
+    display: grid;
+    grid-template-columns: 70% 30%;
+}
+
+button {
+    margin: 5px;
+    font-size: 17px;
+    background-color: #0D0D0D;
+    color: #9FA2A6;
+    border-radius: 6px;
+    border: none;
+    padding: 10px 20px;
+}
+
+button:hover {
+    transform: scale(1.02);
+    transition: 0.2s;
+}
+
+h1,
+h2 {
+    font-size: 35px;
+    padding: 10px;
+    margin-top: 0;
+    color: #0D0D0D;
+    border-bottom: #0D0D0D solid 2px;
 }
 </style>
