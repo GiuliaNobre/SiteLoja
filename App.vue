95dbfa9a7a21241c19c57379d5e4d2d20230594d
GiuliaNobre <106554467+GiuliaNobre@users.noreply.github.com>
2023-05-12 15:09:03 -0300

    Add files via upload

--- App.vue
+++ App.vue
@@ -14,7 +14,7 @@ const produtos = ref([
     nome: 'Camiseta',
     preco: 49.9,
     quantidade: 0,
-    imagem: "imagens/camiseta.png"
+    imagem: "https://img.ltwebstatic.com/images3_pi/2020/12/17/1608191555ab585116fb1b3f892150e679c960f03e_thumbnail_600x.webp"
   },
   {
     id: 2,
@@ -135,52 +135,58 @@ function adicionar(index) {
 
 
 <template>
-  <main class="container1">
-    <div class="produtos-main">
-
+<header class="nav">
       <h2>Loja Dev Web</h2>
+      <button class="btnCarrinho" @click="enviar = true"> Carrinho</button>
+    </header>
 
 
+      <main class="container1" v-if="enviar==false">
+    <div class="produtos-main">
+      
+      <div class="grid">
 
-      <div class="produtos-item" v-for="(produto, index) in produtos" :key="index">
-
-        <div class="colunas-produtos">
-          <div>
-            <p class="info">Imagem: {{ produto.imagem }}</p>
-          </div>
-          <div>
-            <p class="info"> {{ produto.nome }}</p>
-          </div>
-          <div>
-            <p class="info">R$:{{ produto.preco }}</p>
+        <div class="produtos-item" v-for="(produto, index) in produtos" :key="index">
+          
+          <div class="colunas-produtos">
+            <div>
+              <img class="img" :src="produto.imagem" >
+            </div>
+            <div>
+              <p class="info"> {{ produto.nome }}</p>
+            </div>
+            <div>
+              <p class="info">R$:{{ produto.preco }}</p>
           </div>
           <div>
             <p class="info">Valor Total: R$:{{ produto.valorTotal }}</p>
           </div>
-
+          
           <p v-for="(produto, key) in produtos" :key="key">{{ produto.key }}</p>
-
-
+          
+          
           <div class="bt">
             <button class="btProd" @click="produto.quantidade++">+</button>
             <p>{{ produto.quantidade }}</p>
             <button class="btProd" @click="diminuir(produto.id - 1)">-</button>
           </div>
-
-
-          <div>
+          
+          
+        </div>
+          
             <button class="botaoCarrinho" @click="adicionar(index)">Adicionar ao carrinho</button>
-          </div>
+          
         </div>
       </div>
 
-      <button class="btnCarrinho" @click="enviar = true"> Carrinho</button>
-
-      <div v-if="enviar">
+    </div>
+    
+  </main>
+  <div v-else>
         <div class="carrinho">
-          <h2>Carrinho</h2>
+          <h2 class="tituloCarrinho">Carrinho</h2>
 
-          <div class="item" v-for="(item, index) in carrinho" :key="index">
+          <div class="item" v-for="(item, index) in carrinho" :key="index"> 
             <p class="info">Nome: {{ item.nome }}</p>
             <p class="info">Preço: R$:{{ item.preco }}</p>
             <p class="info">Quantidade: {{ item.quantidade }}</p>
@@ -194,26 +200,28 @@ function adicionar(index) {
 
         </div>
       </div>
-
-    </div>
-  </main>
 </template>
 <style scoped>
+
+.grid{
+  
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
 .btnCarrinho {
-  width: 420px;
+  width: 220px;
   background-color: #1d0816;
   color: #fff;
   border: none;
   padding: 10px;
   border-radius: 5px;
   cursor: pointer;
-  margin: 350px;
-  margin-bottom: 0;
-  margin-top: 30px;
+  margin: 100px;
+  margin-bottom: 55px;
+  margin-top: 65px;
   transition: background-color 0.2s, color 0.2s;
 
 }
-
 
 .botaoCarrinho {
   width: 420px;
@@ -239,7 +247,7 @@ li {
   display: inline-flex;
   align-items: center;
   justify-content: space-around;
-  margin: 30px;
+  margin: 10px;
   padding-left: 220px;
 }
 
@@ -263,6 +271,10 @@ li {
 
 }
 
+.nav{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
 .btProd {
   background-color: #200919;
   border: none;
@@ -272,7 +284,6 @@ li {
   border-radius: 5px;
   cursor: pointer;
   margin: 10px;
-
   color: #fff;
 
 }
@@ -283,17 +294,19 @@ li {
 
 h2 {
   margin: 20px;
+  margin-left: 200px;
   text-align: center;
   font-size: 40px;
   font-family: 'Poppins', sans-serif;
   font-style: normal;
-
-
+  margin-top: 60px;
 
 }
 
+
 .produtos-item {
   padding-top: 20px;
+  
 }
 
 .produtos-main .produtos-item {
@@ -301,7 +314,7 @@ h2 {
   background-color: rgba(231, 211, 228, 0.87);
   position: relative;
   width: 500px;
-  margin: 50px;
+  margin: 50px 50px 50px 200px;
   border-radius: 8px;
   box-shadow: 0 4px 18px #2a2f430f;
 
@@ -337,5 +350,9 @@ h2 {
   padding-left: 20px;
   margin-top: 20px;
   transition: background-color 0.2s, color 0.2s;
+}
+.img{
+  width: 200px;
+  margin-left: 150px;
 }
 </style>
